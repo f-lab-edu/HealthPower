a6bad6ac43b1c1716b2f8a1d64e5d1b61158c35f
leeseungeon <oksktmddjs@gmail.com>
2025-06-24 10:07:04 +0900

    docker compose edit

--- src/main/java/com/example/HealthPower/config/WebConfig.java
+++ src/main/java/com/example/HealthPower/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://43.202.51.48:8080")
+                .allowedOrigins("http://3.34.50.165:8080")
                 .allowedOriginPatterns("*") // 개발 중엔 * 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
