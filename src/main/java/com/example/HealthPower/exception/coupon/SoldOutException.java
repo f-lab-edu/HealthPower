9a3edbf8d4c971aea9aeec82007b6c466c78ba01
leeseungeon <oksktmddjs@gmail.com>
2025-07-29 11:00:17 +0900

    deploy edit

--- src/main/java/com/example/HealthPower/exception/coupon/SoldOutException.java
+++
@@ -1,7 +0,0 @@
-package com.example.HealthPower.exception.coupon;
-
-public class SoldOutException extends RuntimeException {
-    public SoldOutException() {
-        super("쿠폰 재고가 없습니다.");
-    }
-}
