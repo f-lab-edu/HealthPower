a2d943b70c5c33e98eb3565ac8246f1e4109a8ba
leeseungeon <oksktmddjs@gmail.com>
2025-08-04 22:08:28 +0900

    redisluaconfig edit

--- healthpower-infra/src/main/java/com/example/infra/redis/RedisLuaConfig.java
+++ healthpower-infra/src/main/java/com/example/infra/redis/RedisLuaConfig.java
@@ -2,18 +2,27 @@ package com.example.infra.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
-import org.springframework.util.ResourceUtils;
-
-import java.io.IOException;
-import java.nio.file.Files;
 
 @Configuration
 public class RedisLuaConfig {
 
+//    @Bean
+//    public RedisScript<Long> claimScript() throws IOException {
+//        String script = Files.readString(ResourceUtils.getFile("classpath:lua/claim.lua").toPath());
+//        return RedisScript.of(script, Long.class);
+//    }
+
     @Bean
-    public RedisScript<Long> claimScript() throws IOException {
-        String script = Files.readString(ResourceUtils.getFile("classpath:lua/claim.lua").toPath());
-        return RedisScript.of(script, Long.class);
+    public RedisScript<Boolean> claimScript() {
+        DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
+
+        // 문제의 원인이었던 부분. getFile() 대신 ClassPathResource 사용
+        redisScript.setLocation(new ClassPathResource("lua/claim.lua"));
+
+        redisScript.setResultType(Boolean.class);
+        return redisScript;
     }
 }
